8c6fd5aee1d139b1fe944320e6aea359b893acfb
Sevak Mardirosian <smardirosians@gmail.com>
2018-04-28 12:02:49 +0200

    Network added.

---
+++ NeuralNetwork.py
@@ -0,0 +1,34 @@
+from keras.models import Sequential
+from keras.layers import Dense, Flatten 
+from rl.policy import EpsGreedyQPolicy
+from rl.agents import SARSAAgent, DQNAgent
+from rl.memory import SequentialMemory
+from keras.layers import Activation 
+
+
+
+class Network:
+	def __init__(self, space, output):
+		super(Network, self).__init__()
+		self.space = space
+		self.output_space = output
+		
+	def model(self):
+		model = Sequential()
+		print("Input shape: "+str((1, ) + self.input_space))
+		model = Sequential()
+		model.add(Flatten(input_shape=(1,) + self.input_shape))
+
+		model.add(Dense(40))
+		mode.add(Activation('relu'))
+
+		model.add(Dense(40))
+		mode.add(Activation('relu'))
+
+		model.add(Dense(self.output_space))
+		mode.add(Activation('linear'))
+
+		return model
+
+
+	
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LunarLander
 Different kinds of experiments using RL on LunarLander 
+
+#Solutions 
+- Using Keras/Tensorflow implemented Q-Learning algorithm  
